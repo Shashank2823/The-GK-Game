19a04dad1a97ce17af2e6dcff7957775f83797d0
Shashank <shashankc171@gmail.com>
2024-04-02 21:24:37 +0530

    testing

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const questions = require('./question.json');
+
 // app.use(express.static(path.join(__dirname, '/'))); // 
 // app.use(express.static("./"))
-const questions = JSON.parse(fs.readFileSync('question.json', 'utf-8'));
-
-// Create a function to replace characters in the answer with underscores
-// function hideAnswer(answer) {
-//     return answer.replace(/[a-zA-Z0-9]/g, '_ ');
-// }
-
+//const questions = JSON.parse(fs.readFileSync('question.json', 'utf-8'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -25,7 +21,6 @@ app.get('/random-question', (req, res) => {
     const randomIndex = Math.floor(Math.random() * questions.length);
     const randomQuestion = questions[randomIndex];
     
-    // Create an object with question and answer with underscores
     const questionWithBlankAnswer = {
         question: randomQuestion.question,
         answer: randomQuestion.answer
